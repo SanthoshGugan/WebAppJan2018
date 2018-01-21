e2e5c5df28e6e1101b719874e0115de9e3c83790
Santhosh Gugan <santhoshg1990@gamil.com>
2018-01-21 20:36:49 +0530

    removed receipe

--- receipeApp/src/app/app.module.ts
+++ receipeApp/src/app/app.module.ts
@@ -3,14 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
-import { ReceipeListComponent } from './receipe-list/receipe-list.component';
 import { HttpModule } from '@angular/http';
+import { AuthInterceptor } from './interceptors/auth.service';
 
 
 @NgModule({
   declarations: [
-    AppComponent,
-    ReceipeListComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,

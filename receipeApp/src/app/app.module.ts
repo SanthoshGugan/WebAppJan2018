ff781dda0bd97a505a405acaa35a736900cc73a9
Santhosh Gugan <santhoshg1990@gamil.com>
2018-01-21 23:29:00 +0530

    Jan 21st night commit

--- receipeApp/src/app/app.module.ts
+++ receipeApp/src/app/app.module.ts
@@ -4,12 +4,15 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HttpModule } from '@angular/http';
-import { AuthInterceptor } from './interceptors/auth.service';
+import { PlaceListComponent } from './place-list/place-list.component';
+import { PlaceItemComponent } from './place-item/place-item.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PlaceListComponent,
+    PlaceItemComponent
   ],
   imports: [
     BrowserModule,
